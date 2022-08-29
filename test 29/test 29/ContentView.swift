64ba99f5481968a2891d768d8f01ad1b79c033ae
Yurii Honcharov <y.honcharov@lampalampa.net>
2022-08-29 16:06:04 +0300

    rerrtr

---
+++ test 29/test 29/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test 29
+//
+//  Created by Yurii Honcharov on 29.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test 29/test 29/test_29App.swift
@@ -0,0 +1,17 @@
+//
+//  test_29App.swift
+//  test 29
+//
+//  Created by Yurii Honcharov on 29.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct test_29App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
